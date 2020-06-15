0c4569f184d818ecade104c5d9f185c0ec3248f9
Enrique Pérez Olivares <enriquejpo@ext.zara.com>
2020-06-15 17:53:13 +0200

    RabbitMQ

--- src/main/java/es/soprasteria/formacion/listener/ConsumerListener.java
+++ src/main/java/es/soprasteria/formacion/listener/ConsumerListener.java
@@ -1,5 +1,6 @@
 package es.soprasteria.formacion.listener;
 
+import es.soprasteria.formacion.dto.SampleMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,12 @@ import org.springframework.stereotype.Component;
 public class ConsumerListener {
 
   @RabbitListener(queues = { "my-queue" })
-  public void receiveMessage(String message) {
-    log.info("Message received: " + message);
+  public void receiveMessage(SampleMessage message) {
+    log.info("Message received from Queue 1: " + message);
+  }
+
+  @RabbitListener(queues = { "my-queue-2" })
+  public void receiveMessage2(SampleMessage message) {
+    log.info("Message received from Queue 2: " + message);
   }
 }
